93c4e049abcff65e124bb2e742ea655058cb7f91
Ari Lerner <arilerner@mac.com>
2010-02-09 00:01:58 -0800

    Remove .os

--- c/parser/y.tab.h
+++ c/parser/y.tab.h
@@ -1,16 +1,87 @@
-#define PHASE 257
-#define HOOK 258
-#define WORD 259
-#define LINE 260
-#define SEMICOLON 261
-#define QUOTE 262
-#define OBRACE 263
-#define EBRACE 264
-#define WHITESPACE 265
-#define COLON 266
-#define PERIOD 267
-typedef union {
+/* A Bison parser, made by GNU Bison 2.3.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+
+   Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2, or (at your option)
+   any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     PHASE = 258,
+     HOOK = 259,
+     WORD = 260,
+     LINE = 261,
+     SEMICOLON = 262,
+     QUOTE = 263,
+     OBRACE = 264,
+     EBRACE = 265,
+     WHITESPACE = 266,
+     COLON = 267,
+     PERIOD = 268
+   };
+#endif
+/* Tokens.  */
+#define PHASE 258
+#define HOOK 259
+#define WORD 260
+#define LINE 261
+#define SEMICOLON 262
+#define QUOTE 263
+#define OBRACE 264
+#define EBRACE 265
+#define WHITESPACE 266
+#define COLON 267
+#define PERIOD 268
+
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+#line 10 "honeycomb.y"
+{
   int i; 
   char* str;
-} YYSTYPE;
+}
+/* Line 1529 of yacc.c.  */
+#line 80 "y.tab.h"
+	YYSTYPE;
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+#endif
+
 extern YYSTYPE yylval;
+
