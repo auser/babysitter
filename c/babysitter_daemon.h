4a8282a98345d8b4145dcf18b9575f78a76013d0
Ari Lerner <arilerner@mac.com>
2010-02-22 19:16:00 -0800

    Splitting out into daemon

---
+++ c/babysitter_daemon.h
@@ -0,0 +1,33 @@
+#ifndef BABYSITTER_H
+#define BABYSITTER_H
+
+#include "honeycomb.h"
+
+//---
+// Helpers
+//---
+const std::string HELP_MESSAGE =
+"Usage:\n"
+" babysitter [-hnD] [-a N]\n"
+"Options:\n"
+" -h                Show this message\n"
+" -n                Using marshaling file descriptors 3 and 4 instead of default 0 and 1\n"
+" -D                Turn on debugging\n"
+" -C                Directory containing the app configuration files.\n"
+" -u [username]     Run as this user (if running as root)\n"
+" -a [seconds]      Set the number of seconds to live after receiving a SIGTERM/SIGINT (default 30)\n"
+" -b [path]         Path to base all the chroot environments on (not yet functional)\n"
+;
+
+//---
+// Variables
+//---
+
+
+//---
+// Functions
+//---
+void  setup_defaults();
+int parse_config_dir();
+
+#endif
