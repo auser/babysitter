0dbdb5854a5efb68d1c4543c20eb0bf9fc3cc369
Ari Lerner <arilerner@mac.com>
2010-03-31 20:32:58 -0700

    More strings test

--- test/tests/TestStringTest.cpp
+++ test/tests/TestStringTest.cpp
@@ -3,6 +3,10 @@
 
 TEST_GROUP(StringTest)
 {
+  char **cargv;
+  int cargc;
+  char *cmd;
+  
   void setup()
   {
   }
@@ -17,5 +21,26 @@ TEST(StringTest, TestingCountArgs)
   LONGS_EQUAL(count_args(""), 0);
   LONGS_EQUAL(count_args("dogs"), 1);
   LONGS_EQUAL(count_args("hello world and fun"), 4);
-  LONGS_EQUAL(count_args("hello world and fun"), 4);
+}
+
+TEST(StringTest, TestArgify)
+{
+  cargc = argify("/bin/bash -c ls -l /var/babysitter", &cargv);
+  LONGS_EQUAL(cargc, 5);
+  STRCMP_EQUAL(cargv[0], "/bin/bash");
+  STRCMP_EQUAL(cargv[1], "-c");
+  STRCMP_EQUAL(cargv[2], "ls");
+  STRCMP_EQUAL(cargv[3], "-l");
+  STRCMP_EQUAL(cargv[4], "/var/babysitter");
+}
+
+TEST(StringTest, TestCommandify)
+{
+  cargc = argify("", &cargv);
+  LONGS_EQUAL(cargc, -1);
+  
+  cargc = argify("/bin/bash -c ls -l /var/babysitter", &cargv);
+  LONGS_EQUAL(cargc, 5);
+  cmd = commandify(cargc, (const char**) cargv);
+  STRCMP_EQUAL(cmd, "/bin/bash -c ls -l /var/babysitter");
 }
