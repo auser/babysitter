eabdcba4cf43baee8538444c2d74291e9276ce0b
Ari Lerner <arilerner@mac.com>
2010-04-01 14:15:36 -0700

    Updated git check

--- test/tests/TestBabysitterUtils.cpp
+++ test/tests/TestBabysitterUtils.cpp
@@ -10,7 +10,7 @@ TEST_GROUP(BabysitterUtils)
 
 TEST(BabysitterUtils, parse_sha_from_git_directory)
 {
-  const char *real_sha = "f6f6641d559fa55c3cb1af0e721c3d1373aa58c9";
+  const char *real_sha = "d5a68aae395b4de3b1c1104cfa621da8a968f9d4";
   const char *git_sha = parse_sha_from_git_directory("../test/fixtures/git_dir");
   STRCMP_EQUAL(git_sha, real_sha);
   STRCMP_EQUAL(parse_sha_from_git_directory("./non/existant/directory"), NULL);
