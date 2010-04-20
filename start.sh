aa67ba2b7c3477fd43048e8d5413878679a995ed
Ari Lerner <arilerner@mac.com>
2010-04-19 22:15:42 -0700

    Moving erlang dir struct

--- start.sh
+++ start.sh
@@ -4,8 +4,7 @@ VERSION=$(cat VERSION | tr -d '\n')
 PWD=$(dirname $0)
 CONFIG=$1
 
-erl -pa $PWD/ebin \
-    -pa deps/*/ebin \
+erl -pa $PWD/erl/ebin \
     -s reloader \
     -babysitter \
     -boot babysitter-$VERSION
