0782e91bcffeb032f701f93ed779dc9d9f7590e6
Ari Lerner <arilerner@mac.com>
2010-04-20 17:55:00 -0700

    Confirmed that killing works with babysitter

--- erl/test/babysitter_test.erl
+++ erl/test/babysitter_test.erl
@@ -40,4 +40,7 @@ test_starting_many_processes() ->
 test_receive_notification_on_death_of_a_process() ->
   {ok, _ErlProcess, Pid} = babysitter:spawn_new("sleep 200.1", [{env, "NAME=ari"}]),
   babysitter:kill_pid(Pid),
-  ?assert(false == "unfinished").
+  CommandArgString = lists:flatten(io_lib:format("ps aux | grep ~p | grep -v grep | wc -l | tr -d ' '", [Pid])),
+  O = ?cmd(CommandArgString),
+  {Int, _} = string:to_integer(O),
+  ?assertEqual(0, Int).
