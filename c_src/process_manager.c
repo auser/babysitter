ca63ffdef84de81d444bbfffba87e76aaeefee2c
Ari Lerner <arilerner@mac.com>
2010-05-26 18:45:18 -0700

    still borked

--- c_src/process_manager.c
+++ c_src/process_manager.c
@@ -334,7 +334,7 @@ pid_t pm_execute(int should_wait, const char* command, const char *cd, int nice,
 int wait_for_pid(pid_t pid)
 {
   if (pid < 0) return pid;
-  // if (kill(pid, 0) == 0) {
+  if (kill(pid, 0) == 0) {
     int childExitStatus;
     waitpid(pid, &childExitStatus, 0);
     // if( !WIFEXITED(childExitStatus) ){
@@ -342,11 +342,10 @@ int wait_for_pid(pid_t pid)
     // } else if (!WIFSTOPPED(childExitStatus)) {
     // }
     return childExitStatus;
-  // } else {
-  //   printf("Something very bad happened with the hook\n");
-  //   return -1;
-  // }
-  
+  } else {
+    printf("Something very bad happened with the hook\n");
+    return -1;
+  }  
 }
 
 typedef enum HookT {BEFORE_HOOK, AFTER_HOOK} hook_t;
@@ -384,9 +383,12 @@ process_return_t* pm_run_and_spawn_process(process_t *process)
   ret->stage = PRS_COMMAND;
   pid_t pid = pm_execute(0, (const char*)process->command, process->cd, (int)process->nice, (const char**)process->env);
   ret->pid = pid;
+  
+  waitpid(pid, &ret->exit_status, WNOHANG);
   if (errno) {
     ret->stderr = (char*)calloc(1, sizeof(char)*strlen(strerror(errno)));
     strncpy(ret->stderr, strerror(errno), strlen(strerror(errno)));
+    return ret;
   }
   if (pid < 0) return ret;
   
@@ -427,13 +429,13 @@ process_return_t* pm_run_process(process_t *process)
   pid_t pid = pm_execute(1, (const char*)process->command, process->cd, (int)process->nice, (const char**)process->env);
   
   ret->pid = pid;
-  ret->exit_status = wait_for_pid(pid);
+  waitpid(pid, &ret->exit_status, WNOHANG);
   if (errno) {
-    printf("command: %s errno: %s\n", process->command, strerror(errno));
     ret->stderr = (char*)calloc(1, sizeof(char)*strlen(strerror(errno)));
     strncpy(ret->stderr, strerror(errno), strlen(strerror(errno)));
     return ret;
   }
+  if (ret->exit_status) return ret;
   
   // Run afterhook
   if (process->after) {
